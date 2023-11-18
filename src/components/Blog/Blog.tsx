dfd06075f13793a7dc2e35c5ef14f5a0b236db76
Dobson Dunavant <dobsonddev@gmail.com>
2023-11-17 19:51:23 -0500

    Update Blog.tsx

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -6,7 +6,7 @@ import blogData from './blogData';
 function Blog() {
     return (
         <div className="flex flex-col items-center bg-custom-orange text-black border-t-8 p-4">
-            <h2 className="text-5xl text-light font-bold underline mb-4">Blog</h2>
+            <h2 className="text-5xl text-white font-bold underline mb-4">Blog</h2>
             {blogData.map((post, index) => (
                 <BlogCard key={index} post={post} />
             ))}
