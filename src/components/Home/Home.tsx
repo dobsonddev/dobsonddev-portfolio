5a9ccfaf20253acaa48ba9a4b81efe5839fb69ff
Dobson Dunavant <dobsonddev@gmail.com>
2023-11-19 01:38:38 -0500

    added accessibility and SEO tags

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Typed from 'react-typed';
 import { FaArrowDown } from 'react-icons/fa'; // Ensure react-icons is installed
+import Head from 'next/head';
 
 function Home() {
     const [showScrollPrompt, setShowScrollPrompt] = useState(false);
@@ -27,6 +28,30 @@ function Home() {
 
     return (
         <div className="relative min-h-screen bg-gradient-to-b from-creamOrange to-softBlue grid grid-flow-row auto-rows-auto items-center justify-items-center px-4">
+            <Head>
+                <title>Dobson Dunavant - Full-Stack Software Engineer, Personal Portfolio Website</title>
+                <meta name="description" content="Dobson Dunavant's full-stack software engineer personal portfolio showcasing skills in web development, data engineering, and more." />
+                <meta name="keywords" content="Dobson Dunavant, portfolio, skills, web development, data engineering" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="robots" content="index, follow" />
+                <meta name="theme-color" content="#your_color_code" />
+                <html lang="en" />
+                <meta name="geo.region" content="US" />
+
+                <meta property="og:title" content="Dobson Dunavant - The SkillSlinger" />
+                <meta property="og:description" content="Dobson Dunavant's portfolio showcasing skills and experiences." />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://dobsond.dev/" />
+                <meta property="og:image" content="https://your-website-url.com/og-image.jpg" />
+                <link rel="canonical" href="https://dobsond.dev/" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Dobson Dunavant - The SkillSlinger" />
+                <meta name="twitter:description" content="Dobson Dunavant's portfolio showcasing skills and experiences." />
+                <meta name="twitter:image" content="https://your-website-url.com/twitter-image.jpg" />
+            </Head>
+
+
             <div className="text-center mt-12 mx-auto">
                 <h1 className="text-4xl md:text-6xl lg:text-8xl font-extrabold tracking-wider text-light leading-loose" style={{ lineHeight: '1.3' }}>
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
