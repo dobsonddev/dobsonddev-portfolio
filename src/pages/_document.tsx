fd08e4f0745066ee7f42350ee9cb8373cabc7b84
Dobson Dunavant <dobsonddev@gmail.com>
2023-11-26 20:33:33 -0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 // pages/_document.tsx
+/* eslint-disable @next/next/no-sync-scripts */
+
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
@@ -11,7 +13,6 @@ class MyDocument extends Document {
                 <body>
                 <Main />
                 <NextScript />
-                {/* Add the Vanta.js scripts here */}
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
                 <script src="https://cdn.jsdelivr.net/npm/vanta/dist/vanta.waves.min.js"></script>
                 </body>
