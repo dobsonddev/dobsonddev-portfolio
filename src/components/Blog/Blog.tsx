31ea3a59d174d7bc3da0606ab1a74c2bb4a904a5
Dobson Dunavant <69472321+dobsonddev@users.noreply.github.com>
2023-12-17 16:49:21 -0500

    contrast fixes

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -6,7 +6,7 @@ import blogData from './BlogData';
 function Blog() {
     return (
         <div className="flex flex-col items-center text-black border-t-8 p-6">
-            <h2 className="text-3xl md:text-4xl lg:text-5xl text-white font-bold underline mb-6">Blog</h2>
+            <h2 className="bg-light rounded p-2 text-3xl md:text-4xl lg:text-5xl font-bold underline mb-6">Blog</h2>
             <div className="w-full flex flex-wrap justify-center">
                 {blogData.map((post, index) => (
                     <BlogCard  key={index} post={post} />
