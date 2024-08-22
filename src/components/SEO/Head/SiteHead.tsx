2110a37719a2456aaae8d9f8e593062b508dd301
Dobson Dunavant <69472321+dobsonddev@users.noreply.github.com>
2024-08-22 15:37:16 -0400

    Update SiteHead.tsx

--- src/components/SEO/Head/SiteHead.tsx
+++ src/components/SEO/Head/SiteHead.tsx
@@ -23,7 +23,6 @@ const SiteHead: React.FC<SiteHeadProps> = ({ title, description, author, keyword
             <meta name="keywords" content={keywords}/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta name="robots" content="index, follow"/>
-            <link rel="canonical" href="https://www.dobsond.dev/Dobson_Dunavant_Resume.pdf"/>
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
